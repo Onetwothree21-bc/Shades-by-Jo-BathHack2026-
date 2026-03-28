6ef5244bdde9520e18ca2cf63397bc0fc25b1c22
tomjones1710 <ohazard1710@icloud.com>
2026-03-28 23:49:28 +0000

    Add example dialogue and update text processing in extract_info.py

--- extract_info.py
+++ extract_info.py
@@ -10,43 +10,71 @@ nltk.download('maxent_ne_chunker_tab')
 nltk.download('averaged_perceptron_tagger_eng')
 import speech_recognition as sr
 
-text = open('examples.txt', 'r').read()
+text = open('example2.txt', 'r').read()
 #tokenizer = RegexpTokenizer(r'\w+')
 text = re.sub(r'[^\x00-\x7F]+', '', text)
 #text = re.sub(r'[^\w\s]', '', text)
 grammar = r"""
 COURSE: {<STUDY>.*<NNP>+}
-        {<STUDY>.*<NN>+},
+        {<STUDY>.*<NN>+}
+        {<STUDY>.*<COURSE>+},
 NAME: {<NAME><NNP>+}
         {<NAME><NN>+}"""
+
+courses_list = [
+    "architecture", "biomed", "biology", "bio", "business", "management", "chem eng", "chemistry", "civil eng", "computer science", "cs", "electrical eng", "ee", "engineering", "english", "history", "maths", "mathematics", "medicine", "physics", "psychology", "social science", "sociology", "statistics", "MSDS", "economics", "econometrics", "finance", "accounting", "marketing", "operations research", "data science", "artificial intelligence", "machine learning", "deep learning", "natural language processing", "computer vision", "cybersecurity", "software engineering", "web development", "mobile app development", "game development", "cloud computing", "big data", "blockchain", "cryptography", "econ", "mech eng", "languages", "physics", "politics", "sports science", "sport management"
+]
+
+
+
+
+
 
 def preprocess(text):
     #sentences = nltk.sent_tokenize(text)
     #sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
-    sentences = [nltk.word_tokenize(text)]
-    sentences = [nltk.pos_tag(sentence) for sentence in sentences]
+    sentences = []
+    for line_number, line in enumerate(text.splitlines()):
+        id = line.split(':')[0]
+        line = line.split(':')[1]
+        sentence = nltk.word_tokenize(line)
+        sentence = nltk.pos_tag(sentence)
+        sentence2 = []
+        for word, tag in sentence:
+            sentences.append((word, tag, id))
+        #sentences.append(sentence2)
+    #sentences = [nltk.word_tokenize(text)]
+    #sentences = [nltk.pos_tag(sentence) for sentence in sentences]
     return sentences
 
 
 
 preprocessed_text = preprocess(text)
-preprocessed_text = [
-    (word, "STUDY") if (word.lower() == "study") | (word.lower == "studies") | (word.lower == "studying") | (word.lower == "course") else (word, tag)
-    for (word, tag) in preprocessed_text[0]
-]
+#preprocessed_text = [
+#    (word, "STUDY") if (word.lower() == "study") | (word.lower == "studies") | (word.lower == "studying") | (word.lower == "course") else (word, tag)
+#    for (word, tag, id) in preprocessed_text[0]
+#]
 text1 = []
-for (word, tag) in preprocessed_text:
+for (word, tag, id) in preprocessed_text:
     if ((word == "study") | (word == "studies") | (word == "studying") | (word == "course") | (word == "doing")):
-        text1.append((word, "STUDY"))
+        text1.append((word, "STUDY", id))
     elif ((word == "name") | (word == "Im")):
-        text1.append((word, "NAME"))
+        text1.append((word, "NAME", id))
+    elif (word in courses_list):
+        text1.append((word, "COURSE", id))
     else:
-        text1.append((word, tag))
+        text1.append((word, tag, id))
 
 result = []
 cp = nltk.RegexpParser(grammar)
 
 result = cp.parse(text1)
+
+names = []
+for subtree in result.subtrees():
+    if subtree.label() == 'NAME':
+        names.append(subtree.leaves()[1][0])
+
 
 #for sentence in preprocessed_text:
  #   result1 = cp.parse(sentence)
