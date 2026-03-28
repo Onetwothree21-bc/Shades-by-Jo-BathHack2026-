7542eb20d6dd6b9ecc392cce3712660290590cb1
tomjones1710 <ohazard1710@icloud.com>
2026-03-28 17:07:59 +0000

    Add examples.txt and implement text preprocessing in extract_info.py

--- extract_info.py
+++ extract_info.py
@@ -1,2 +1,21 @@
 import nltk
+from nltk.tokenize import RegexpTokenizer
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger_eng')
 import speech_recognition as sr
+
+text = open('examples.txt', 'r').read()
+tokenizer = RegexpTokenizer(r'\w+')
+
+def preprocess(text):
+    sentences = tokenizer.tokenize(text)
+    sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
+    sentences = [nltk.pos_tag(sentence) for sentence in sentences]
+    return sentences
+
+
+
+preprocessed_text = preprocess(text)
+
+
+a = 4
